ac0cbfe97cf40eb498545144a5ce4b4cf57528dd
BickhamM21 <BickhamM21@ehsbr.org>
2020-01-27 20:36:40 -0600

    Added PID driveTrain code, not finished just yet

--- src/main/java/frc/robot/subsystems/driveTrain.java
+++ src/main/java/frc/robot/subsystems/driveTrain.java
@@ -10,8 +10,10 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.commands.driveTrainCommand;
 
 public class driveTrain extends SubsystemBase {
 
@@ -39,6 +41,11 @@ public class driveTrain extends SubsystemBase {
     new WPI_TalonFX(4),
     new WPI_TalonFX(5),
   };
+  
+
+  double leftTargetVelocity;
+  double rightTargetVelocity; 
+  public double PIDMultiplier = 6000 * 2048 / 600;
 
   /**
    * Creates a new driveTrain.
@@ -49,10 +56,6 @@ public class driveTrain extends SubsystemBase {
     for(int i = 0; i < 6; i++) {
       driveTrainMotors[i].configFactoryDefault();
 
-      //Talon SRX
-      //driveTrainMotors[i]
-
-      //Talon FX
     }
 
     //Set each of the back two motors to follow the lead motor 
@@ -85,10 +88,26 @@ public class driveTrain extends SubsystemBase {
 		driveTrainMotors[3].config_kI(Constants.kPIDLoopIdx, Constants.kGains_Velocit.kI, Constants.kTimeoutMs);
 		driveTrainMotors[3].config_kD(Constants.kPIDLoopIdx, Constants.kGains_Velocit.kD, Constants.kTimeoutMs);
 
+
+
+
+    CommandScheduler.getInstance().setDefaultCommand(this, new driveTrainCommand(this));
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  
+
+  public void arcadeDrivePID(double throttle, double rot) {
+    double leftPow = throttle + rot;
+    double rightPow = throttle - rot;
+
+    driveTrainMotors[0].set(ControlMode.Velocity, leftPow * PIDMultiplier);
+    driveTrainMotors[3].set(ControlMode.Velocity, rightPow * PIDMultiplier);
+
+
   }
 }
